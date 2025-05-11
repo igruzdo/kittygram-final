e510fd897fa07f195ed898d4bf6d3f9882c73a15
Игорь Груздо <igor_gruzdo@bellintegrator.com>
2025-05-11 17:10:59 +0530

    done

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,5 @@
 output "vm_ip" {
   description = "Public IP-адрес виртуальной машины для деплоя"
   value       = yandex_compute_instance.vm.network_interface[0].nat_ip_address
-  sensitive   = false  # Можно установить true, если нужно скрыть в логах
+  sensitive   = false
 }
