c6e5c062fe53662aa697a6bd63e0b780a29fcf5d
Игорь Груздо <igor_gruzdo@bellintegrator.com>
2025-05-11 19:13:17 +0530

    done

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,3 +3,9 @@ output "vm_ip" {
   value       = yandex_compute_instance.vm.network_interface[0].nat_ip_address
   sensitive   = false
 }
+
+output "ssh_public_key" {
+  description = "Public IP-адрес виртуальной машины для деплоя"
+  value       = yandex_compute_instance.vm.metadata.ssh-keys
+  sensitive   = false
+}
